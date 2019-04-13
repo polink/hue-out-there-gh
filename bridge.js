4e255d371c5192629854fc6dd7e0a7d8497c74ff
sarkis74 <sarkis_aghazarian@yahoo.com>
2019-04-13 09:46:25 -0700

    pulling latest

--- bridge.js
+++ bridge.js
@@ -3,7 +3,6 @@
 let hue = require('node-hue-api');
 let HueApi = hue.HueApi;
 
-let huescene = hue.scene
 let lightState = require('node-hue-api').lightState;
 
 let displayResults = function(result) {
@@ -20,14 +19,15 @@ let api = new HueApi(host, username);
 let sceneId =  "60VeO3KNkG1aec9";
 let state = lightState.create();
 
+api = new HueApi(host, username);
+api.setLightState(4, state.on(), function(err, result) {
+    if (err) throw err;
+    displayResult(result);
+});
 
-api.setLightState(1, state.off())
-api.setLightState(2, state.off())
-    .then(displayResults)
-    .fail(displayError)
-    .done();
+// console.log(process.argv.slice(2)[0])
 
-console.log(process.argv.slice(2)[0])
+
 
 
 
